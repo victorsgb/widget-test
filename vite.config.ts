950365938621c9dffea28824db619f7af1ee328a
Victor Baptista <victorsgb@gmail.com>
2025-07-25 09:35:53 -0300

    fix: project was unavailable in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,23 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-// Resolve __dirname equivalent
-const __filename = fileURLToPath(import.meta.url as unknown as URL);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
     host: '0.0.0.0',
     port: 5004,
+    // allowedHosts: [
+    //   'monitdb-dev.ddns.net',
+    //   '127c-187-19-224-237.ngrok-free.app'
+    // ],
     strictPort: true,
     hmr: {
       clientPort: 5004
-    }
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/widget.tsx'),
-      name: 'TawkeeWidget',
-      fileName: (format) => `chat-widget.${format}.js`,
-      formats: ['iife']
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
     }
   }
 });
